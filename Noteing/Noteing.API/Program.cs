814985e11b4f6b273798cbcdac17bf181c822583
gerbenyielddd <g.van.de.wiel@yielddd.com>
2024-09-17 08:38:45 +0200

    fix remove secrets

--- Noteing/Noteing.API/Program.cs
+++ Noteing/Noteing.API/Program.cs
@@ -13,7 +13,7 @@ using Noteing.API.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // This shouldn't be seen by anyone.
-// Good that you guys are joining tonight! The secrets for today is YLD{Welkom2024!Secr3t}
+// Good that you guys are joining tonight! The secrets for today is ******************
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("ConnString");
